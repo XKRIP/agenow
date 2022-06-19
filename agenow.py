a001f56c8fa4803c643131b2df72fc0152cad538
XKRIP <107795252+XKRIP@users.noreply.github.com>
2022-06-19 17:30:17 +0530

    Add files via upload

---
+++ agenow.py
@@ -0,0 +1,16 @@
+#This is simple python tool for current age calculator
+#author - XKRIP
+print("__________________________")
+print(" W E L C O M E   T O   A G E N O W")
+print()
+print()
+name = str(input("ENTER YOUR NAME : ")
+print("__________________________")
+print("Hello!", name)
+print("__________________________")
+a = int(input("ENTER CURRENT YEAR : ")
+b = int(input("ENTER BIRTH YEAR : ")
+#Formula for this a - b = Current Age
+out = a - b
+print("You are : ", out, "year old", name)
+exit()
